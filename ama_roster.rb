2e7b0687a3c7a2376726b49cadc85d05afd7001d
Adam Bray <adam.bray@yale.edu>
2015-03-16 16:08:28 -0400

    balanced out 1:1 assignments

--- ama_roster.rb
+++ ama_roster.rb
@@ -6,6 +6,6 @@ ama_students = %w( Rodger Kevin Tyler Alif Isa Damon Zakk Brad Joe Jocelyn Diana
 # andy = ama_students.each_slice(7).to_a[1]
 # matt = ama_students.each_slice(7).to_a[2]
 
-adam = %w(Noah John Jocelyn Damon Ammie Alif Karen)
-andy = %w(Phil Tyler Biboy Brad Rodger Tammy Allyson Nic)
+adam = %w(Noah John Jocelyn Damon Ammie Alif Karen Nic)
+andy = %w(Phil Tyler Biboy Brad Rodger Tammy Allyson)
 matt = %w(Don Zakk Kevin Diana Jamey Joe Isa)
