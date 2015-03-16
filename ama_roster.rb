280ce308b33364e60025471730dcadacbea88807
Adam Bray <adam.bray@yale.edu>
2015-03-16 16:00:57 -0400

    added 1:1 pairings, lesson plan for git

---
+++ ama_roster.rb
@@ -0,0 +1,11 @@
+ama_students = %w( Rodger Kevin Tyler Alif Isa Damon Zakk Brad Joe Jocelyn Diana Noah Allyson Ammie Jamey Don John Karen Tammy Biboy Nic Phil)
+
+# ama_students.shuffle!
+#
+# adam = ama_students.each_slice(8).to_a[0]
+# andy = ama_students.each_slice(7).to_a[1]
+# matt = ama_students.each_slice(7).to_a[2]
+
+adam = %w(Noah John Jocelyn Damon Ammie Alif Karen)
+andy = %w(Phil Tyler Biboy Brad Rodger Tammy Allyson Nic)
+matt = %w(Don Zakk Kevin Diana Jamey Joe Isa)
