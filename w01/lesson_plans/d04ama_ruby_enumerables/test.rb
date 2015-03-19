ef3042cda5090c28f4b638e21251415efe8b45be
Andy <andrew.sunglae.kim@gmail.com>
2015-03-18 21:20:01 -0400

    rm test.rb

--- w01/lesson_plans/d04ama_ruby_enumerables/test.rb
+++
@@ -1,9 +0,0 @@
-def upcase1 str
-  str.split("").map{|char|char = "a"}.join("")
-end
-
-puts "bob".upcase
-puts "the method above is using the ruby method"
-
-puts " the method below is using my method that i created"
-puts upcase1 "bob"
