c4ad3fb565aed9ee810e0186e7b0bc2767bd91f9
mattscilipoti <matt.scilipoti@ga.co>
2015-03-20 10:47:30 -0400

    bdd: extract specs for scrabble_scorer.rb

--- w01/lesson_plans/d05_bdd/bdd_scrabble_scorer_starter.rb
+++ w01/lesson_plans/d05_bdd/bdd_scrabble_scorer_starter.rb
@@ -22,12 +22,12 @@
 # Important Help:
 # This file has the code and the specs together.
 # You can run the specs using
-#    $ rspec --color --fd scrabble_scorer_assignment.rb --specs
+#    $ rspec bdd_scrabble_scorer_starter_spec.rb
 
 # Run the scorer, using:
-#    $ ruby scrabble_scorer_assignment.rb any_word
+#    $ ruby bdd_scrabble_scorer_starter.rb any_word
 #  e.g.:
-#    $ ruby scrabble_scorer_assignment.rb street #=> 6
+#    $ ruby bdd_scrabble_scorer_starter.rb street #=> 6
 
 # Scrabble(c) Letter Scores:
 # A: 1
@@ -68,56 +68,7 @@ end
 
 #<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
-def should_run_specs?
-  $PROGRAM_NAME.end_with?('rspec') # we ran this file via `rspec`
-end
-
-unless should_run_specs?
-  # score using the passed word
-  passed_word = ARGV[0]
-  score = scrabble_scorer(passed_word)
-  puts "The word '#{passed_word}' scores: #{score}"
-
-else
-  # run the specs
-
-  RSpec.describe 'scrabble_scorer' do
-    it "scores zero for empty word" do
-      expect(scrabble_scorer("")).to eql(0)
-    end
-
-    # it "scores zero for nil" do
-    #   expect(scrabble_scorer(nil)).to eql(0)
-    # end
-    #
-    # it "scores the letter 'a' as 1" do
-    #   expect(scrabble_scorer('a')).to eql(1)
-    # end
-    #
-    # it "scores the letter 'f' as 4" do
-    #   expect(scrabble_scorer('f')).to eql(4)
-    # end
-    #
-    # it "scores a very short word" do
-    #   expect(scrabble_scorer('as')).to eql(1+1)
-    # end
-    #
-    # it "scores a simple word 'street'" do
-    #   expect(scrabble_scorer('street')).to eql(6) # 1+1+1+1+1+1
-    # end
-    #
-    # it "scores a complicated word 'quirky'" do
-    #   expect(scrabble_scorer('quirky')).to eql(22)
-    # end
-  end
-
-  # describe "bonus" do
-  #   it "scores zero for whitespace" do
-  #     expect(scrabble_scorer(" \t\n")).to eql(0)
-  #   end
-  #
-  #   it "is not case-sensitive" do
-  #     expect(scrabble_scorer("STReeT")).to eql(6)
-  #   end
-  # end
-end
+# score using the passed word
+passed_word = ARGV[0]
+score = scrabble_scorer(passed_word)
+puts "The word '#{passed_word}' scores: #{score}"
