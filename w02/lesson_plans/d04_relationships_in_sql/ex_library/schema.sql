2ca02fbbd022c7a6d8e8c77c173d4e1c114b96eb
Adam Bray <adam.bray@yale.edu>
2015-03-26 22:04:22 -0400

    updates to notes on sql

--- w02/lesson_plans/d04_relationships_in_sql/ex_library/schema.sql
+++ w02/lesson_plans/d04_relationships_in_sql/ex_library/schema.sql
@@ -2,15 +2,9 @@ DROP TABLE IF EXISTS books;
 DROP TABLE IF EXISTS authors;
 
 CREATE TABLE authors (
-  id INTEGER PRIMARY KEY,
-  name VARCHAR(255),
-  nationality VARCHAR(255),
-  birth_year INTEGER
+  id SERIAL PRIMARY KEY,
 );
 
 CREATE TABLE books (
-  id INTEGER PRIMARY KEY,
-  title VARCHAR(255),
-  publication_date INTEGER,
-  author_id INTEGER
+  id  SERIAL PRIMARY KEY,
 );
