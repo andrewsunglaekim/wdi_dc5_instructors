0e5d0f7893c7af446c32a8358e044934f6609b69
Adam Bray <adam.bray@yale.edu>
2015-03-26 22:06:04 -0400

    reverted deletion of schema

--- w02/lesson_plans/d04_relationships_in_sql/ex_library/schema.sql
+++ w02/lesson_plans/d04_relationships_in_sql/ex_library/schema.sql
@@ -2,9 +2,15 @@ DROP TABLE IF EXISTS books;
 DROP TABLE IF EXISTS authors;
 
 CREATE TABLE authors (
-  id SERIAL PRIMARY KEY,
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(255),
+  nationality VARCHAR(255),
+  birth_year INTEGER
 );
 
 CREATE TABLE books (
-  id  SERIAL PRIMARY KEY,
+  id INTEGER PRIMARY KEY,
+  title VARCHAR(255),
+  publication_date INTEGER,
+  author_id INTEGER
 );
