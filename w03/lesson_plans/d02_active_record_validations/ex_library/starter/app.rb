3cca6ec51d32809b5f6f329c50e84d2958205950
Adam Bray <adam.bray@yale.edu>
2015-03-31 01:08:21 -0400

    added validations lesson

---
+++ w03/lesson_plans/d02_active_record_validations/ex_library/starter/app.rb
@@ -0,0 +1,8 @@
+require 'active_record'
+require 'pry'
+
+require_relative 'db/connection'
+require_relative 'lib/author'
+require_relative 'lib/book'
+
+binding.pry
---
+++ w03/lesson_plans/d02_active_record_validations/ex_library/starter/db/connection.rb
@@ -0,0 +1,4 @@
+ActiveRecord::Base.establish_connection(
+  database: "library",
+  adapter:  "postgresql"
+)
